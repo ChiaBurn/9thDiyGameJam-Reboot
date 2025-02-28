399f08451c44cffd4b37dc902bf391806867257f
ChiaBurn <devchiaburn@gmail.com>
2025-02-28 09:55:51 +0000

    Show sentence gradually.

--- Assets/Scripts/PlotManager.cs
+++ Assets/Scripts/PlotManager.cs
@@ -1,18 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlotManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Text dialogueText;
+    [SerializeField]
+    float secondsBetweenChar = 0.05f;
+
+    private Queue<string> sentencesToShow;
+
     void Start()
     {
-        
+        sentencesToShow = new Queue<string>();
+
+        var testSentences = new string[]
+        {
+            "第一句話第一句話第一句話第一句話第一句話第一句話第一句話",
+            "第二句話第二句話第二句話第二句話第二句話第二句話第二句話第二句話",
+            "最後一句話最後一句話最後一句話最後一句話最後一句話最後一句話最後一句話"
+        };
+
+        StartDialogue(testSentences);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartDialogue(IEnumerable<string> sentences)
     {
-        
+        sentencesToShow.Clear();
+        foreach (var sentence in sentences)
+        {
+            sentencesToShow.Enqueue(sentence);
+        }
+
+        ShowNextSentence();
+
     }
+
+    public void ShowNextSentence()
+    {
+        if (!sentencesToShow.Any())
+        {
+            EndDialogue();
+            return;
+        }
+
+        var sentenceIsShowing = sentencesToShow.Dequeue();
+        StopCoroutine(nameof(TypeSentence));
+        StartCoroutine(nameof(TypeSentence), sentenceIsShowing);
+
+
+    }
+
+    private IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (var c in sentence.ToCharArray())
+        {
+            yield return new WaitForSeconds(secondsBetweenChar);
+            dialogueText.text += c;
+        }
+    }
+
+    private void EndDialogue()
+    {
+        Debug.Log("End Dialogue");
+    }
+
+
 
 
     public void GoMainMenu()
