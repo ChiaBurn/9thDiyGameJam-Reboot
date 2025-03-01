570fd21dfa5daa686e355e8cd845c1455b95b63d
ChiaBurn <devchiaburn@gmail.com>
2025-03-01 23:26:18 +0000

    Add buttons to change status for testing.

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,15 +1,69 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public Text FinishedLevelConutText;
+    public Text chapText;
+    public Text levelText;
+    public Text statusText;
+    public Text scoreText;
+    private PersistentManager manager;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
+        manager = PersistentManager.Instance;
+        chapText.text = $"Chap: {manager.currentChapter}";
+        levelText.text = $"Level: {manager.currentLevel}";
+        statusText.text = $"Status: {manager.currentLevelStatus}";
+        scoreText.text = $"Score: {manager.currentChapterScore}";
     }
+
+    public void ChangeChap()
+    {
+        manager.currentChapter = manager.currentChapter switch
+        {
+            1 => 2,
+            2 => 1,
+            _ => 1
+        };
+        chapText.text = $"Chap: {manager.currentChapter}";
+    }
+    public void ChangeLevel()
+    {
+        manager.currentLevel = manager.currentLevel switch
+        {
+            1 => 2,
+            2 => 3,
+            3 => 1,
+            _ => 1
+        };
+        levelText.text = $"Level: {manager.currentLevel}";
+    }
+    public void ChangeStatus()
+    {
+        manager.currentLevelStatus = manager.currentLevelStatus switch
+        {
+            LevelStatusEnum.Begin => LevelStatusEnum.Failed,
+            LevelStatusEnum.Failed => LevelStatusEnum.Successed,
+            LevelStatusEnum.Successed => LevelStatusEnum.Begin,
+            _ => LevelStatusEnum.Begin
+        };
+        statusText.text = $"Status: {manager.currentLevelStatus}";
+    }
+    public void ChangeScore()
+    {
+        manager.currentChapterScore = manager.currentChapterScore switch
+        {
+            0 => 1,
+            1 => 2,
+            2 => 3,
+            3 => 0,
+            _ => 0
+        };
+        scoreText.text = $"Score: {manager.currentChapterScore}";
+    }
+
 
 
     public void GoChap02()
