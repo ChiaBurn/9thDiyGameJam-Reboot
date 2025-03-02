b8693e8e2029b447717ffcf0d9b75935b90df11e
ChiaBurn <devchiaburn@gmail.com>
2025-03-02 13:43:28 +0000

    Allow wrong answer.

--- Assets/Scripts/JigsawManager.cs
+++ Assets/Scripts/JigsawManager.cs
@@ -249,7 +249,7 @@ public class JigsawManager : MonoBehaviour
                 bool isCorrect = empty == targetPosition;
                 isSuccess &= isCorrect;
 
-                 if (isCorrect) 
+                if (isCorrect)
                 {
                     // Turn Pieces gray
                     MeshRenderer meshRenderer = draggingPiece.GetComponent<MeshRenderer>();
@@ -259,15 +259,16 @@ public class JigsawManager : MonoBehaviour
                         meshRenderer.material.SetColor("_Color", new Color(0.7f, 0.7f, 0.7f, 1f)); // gray
                     }
 
-                    // Disable the collider so we can't click on the object anymore.
-                    draggingPiece.GetComponent<BoxCollider2D>().enabled = false;
-                    emptyPositions.Remove(empty);
-                    // Increase the number of correct pieces, and check for puzzle completion.
-                    if (!emptyPositions.Any())
-                    {
-                        StartCoroutine(EndGame());
-                    }
-                    
+                }
+
+
+                // Disable the collider so we can't click on the object anymore.
+                draggingPiece.GetComponent<BoxCollider2D>().enabled = false;
+                emptyPositions.Remove(empty);
+                // Increase the number of correct pieces, and check for puzzle completion.
+                if (!emptyPositions.Any())
+                {
+                    StartCoroutine(EndGame());
                 }
                 break;
             }
